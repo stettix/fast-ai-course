561b00ffa3b25eb979da2ad1e87e29a1742cac6e
stettix <jan.stette@gmail.com>
2017-09-22 15:25:51 +0100

    Add initial notebook + helper code for classifying images using Keras

---
+++ notebooks/deeplearning1/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def plot_images(ims, figsize=(12, 6), rows=1, interp=False, titles=None):
+    """
+    Given an array of images, lays out and plots these, including labels if given.
+    Adapts images given as numpy data to the format used by matplotlib.
+    """
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
+        if (ims.shape[-1] != 3):
+            ims = ims.transpose((0, 2, 3, 1))
+    f = plt.figure(figsize=figsize)
+    cols = len(ims) // rows if len(ims) % 2 == 0 else len(ims) // rows + 1
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows, cols, i + 1)
+        sp.axis('Off')
+        if titles is not None:
+            sp.set_title(titles[i], fontsize=16)
+        plt.imshow(ims[i], interpolation=None if interp else 'none')
